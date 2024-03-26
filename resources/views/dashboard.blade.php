abbe0db158948ff5b7cc23fdb1495352d0297964
adinfahru <mohamad.fahrudin18@gmail.com>
2024-03-27 01:07:01 +0700

    update navigation for admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-    {{ __('Welcome!, ') }} {{ Auth::user()->name }} {{ __(' Dashboard') }}
+    {{ __('Welcome!, ') }} {{ Auth::user()->name }}
     </x-slot>
 
    
