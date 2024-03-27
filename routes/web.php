ec84f775d5c7aa7dad301ad6a99d17b70adb749a
adinfahru <mohamad.fahrudin18@gmail.com>
2024-03-28 05:38:12 +0700

    create user controller

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'admin'])->group(function () {
     Route::view('about', 'about')->name('about');
 
     Route::get('users', [UserController::class, 'index'])->name('users.index')->
-    middleware(['auth', 'admin']);;
+    middleware(['auth']);;
 
     Route::get('admin', [AdminController::class, 'index'])->name('admin.index')->
-    middleware(['auth', 'admin']);;;
+    middleware(['auth']);;;
 
     Route::resource('products', ProductsController::class)->
-    middleware(['auth', 'admin']);;;
+    middleware(['auth']);;;
+
+    Route::resource('users', UserController::class)->
+    middleware(['auth']);;;
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
