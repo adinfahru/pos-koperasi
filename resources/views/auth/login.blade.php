a79a6a3504f12b96cc0b4601e9dc72604d2551ee
rayhanalr <rayhanalrasyid10@gmail.com>
2024-03-30 22:02:05 +0700

    Latest update login fiture

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,35 @@
+<nav>
+    <div class="justify-center w-full mx-auto">
+        <div class="flex flex-col w-full px-8 py-2 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
+            <div class="flex flex-row items-center justify-between text-black">
+                <a class="inline-flex items-center gap-3 text-xl font-bold tracking-tight text-indigo-800" href="/">
+                    <span>Koperasi Jaya</span>
+                </a>
+            </div>
+            <div class="flex-col items-center gap-16 text-sm font-medium text-indigo-800 md:flex md:flex-row lg:p-0 md:mt-0">
+                <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Beranda</a>
+                <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Profile</a>
+                <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Info Berita</a>
+                <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Produk</a>
+                <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Bantuan</a>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="justify-center w-full mx-auto bg-indigo-800">
+        <div class="flex flex-col w-full px-8 py-2.5 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
+        <p class="text-xs text-white">
+                Jl. Telekomunikasi No. 5, Terusan Buah Batu, Bandung, 40257
+            </p>
+        </div>
+    </div>
+</nav>
 <x-guest-layout>
-    <div class="flex flex-col overflow-y-auto md:flex-row">
-        <div class="h-32 md:h-auto md:w-1/2">
-            <img aria-hidden="true" class="object-cover w-full h-full"
-                 src="{{ asset('images/login-office.jpeg') }}"
-                 alt="Office"/>
+    <div class="flex flex-col overflow-y-auto md:flex-row sm:flex-col sm:gap-10">
+        <div class="md:h-auto md:w-1/2 shadow-2xl rounded-xl sm:shadow-md">
+            <img aria-hidden="true" class="object-cover w-full h-full rounded-lg"
+                src="{{ asset('images/Koperasi-Logo.png') }}" alt="Office" />
+                
         </div>
         <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
             <div class="w-full">
@@ -16,32 +42,23 @@
 
                     <!-- Input[ype="email"] -->
                     <div class="mt-4">
-                        <x-input-label :value="__('Email')"/>
-                        <x-text-input type="email"
-                                 id="email"
-                                 name="email"
-                                 value="{{ old('email') }}"
-                                 class="block w-full"
-                                 required
-                                 autofocus/>
+                        <x-input-label :value="__('Email')" />
+                        <x-text-input type="email" id="email" name="email" value="{{ old('email') }}"
+                            class="block w-full" required autofocus />
                         <x-input-error :messages="$errors->get('email')" class="mt-2" />
                     </div>
 
                     <!-- Input[ype="password"] -->
                     <div class="mt-4">
-                        <x-input-label for="password" :value="__('Password')"/>
-                        <x-text-input type="password"
-                                 id="password"
-                                 name="password"
-                                 class="block w-full"/>
+                        <x-input-label for="password" :value="__('Password')" />
+                        <x-text-input type="password" id="password" name="password" class="block w-full" />
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
 
                     <div class="flex mt-6 text-sm">
                         <label class="flex items-center dark:text-gray-400">
-                            <input type="checkbox"
-                                   name="remember"
-                                   class="text-purple-600 form-checkbox focus:border-purple-400 focus:outline-none focus:shadow-outline-purple">
+                            <input type="checkbox" name="remember"
+                                class="text-indigo-800 form-checkbox focus:border-purple-400 focus:outline-none focus:shadow-outline-purple">
                             <span class="ml-2">{{ __('Remember me') }}</span>
                         </label>
                     </div>
@@ -53,16 +70,25 @@
                     </div>
                 </form>
 
-                <hr class="my-8"/>
+                <hr class="my-8" />
 
-                @if (Route::has('password.request'))
-                    <p class="mt-4">
+                <div class="flex flex-col items-center gap-4">
+
+                    @if (Route::has('password.request'))
+                    <p class="">
                         <a class="text-sm font-medium text-primary-600 hover:underline"
-                           href="{{ route('password.request') }}">
+                            href="{{ route('password.request') }}">
                             {{ __('Forgot your password?') }}
                         </a>
                     </p>
-                @endif
+                    @endif
+
+                    <a href="/"
+                        class="text-white bg-purple-600 hover:bg-purple-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 dark:bg-blue-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
+                        Home
+                    </a>
+
+                </div>
             </div>
         </div>
     </div>
