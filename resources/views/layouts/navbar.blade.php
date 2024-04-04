fba15160590fa3be8cd494bc021b3fa359a3f0b9
adinfahru <mohamad.fahrudin18@gmail.com>
2024-04-05 06:03:12 +0700

    update layout navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav>
+    <div class="justify-center w-full mx-auto">
+        <div class="flex flex-col w-full px-8 py-2 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
+            <div class="flex flex-row items-center justify-between text-black">
+                <a class="inline-flex items-center gap-3 text-lg font-bold tracking-tight text-indigo-800" href="/">
+                    <span>Koperasi Jaya</span>
+                </a>
+            </div>
+            <div class="flex-col items-center text-xs gap-12 text-sm font-medium text-indigo-800 md:flex md:flex-row lg:p-0 md:mt-0">
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Beranda</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Profile</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Info Berita</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Produk</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="{{route('register')}}">Register</a>
+                <a href="{{route('login')}}">
+                    <button type="button" class="rounded-md bg-indigo-800 px-6 py-1.5 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Masuk
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="justify-center w-full mx-auto bg-indigo-800">
+        <div class="flex flex-col w-full px-8 py-1 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-1xl">
+            <p class="text-xs text-white">
+                Jl. Telekomunikasi No. 5, Terusan Buah Batu, Bandung, 40257
+            </p>
+        </div>
+    </div>
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,47 +10,11 @@
 </head>
 
 <body class="antialiased">
-    <nav>
-        <div class="justify-center w-full mx-auto">
-            <div class="flex flex-col w-full px-8 py-2 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
-                <div class="flex flex-row items-center justify-between text-black">
-                    <a class="inline-flex items-center gap-3 text-xl font-bold tracking-tight text-indigo-800" href="/">
-                        <span>Koperasi Jaya</span>
-                    </a>
-                </div>
-                <div class="flex-col items-center gap-16 text-sm font-medium text-indigo-800 md:flex md:flex-row lg:p-0 md:mt-0">
-                    <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Beranda</a>
-                    <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Profile</a>
-                    <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Info Berita</a>
-                    <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Produk</a>
-                    <a class="hover:text-black focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Bantuan</a>
-                    <a href="{{route('login')}}">
-                        <button type="button" class="rounded-md bg-indigo-800 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Masuk
-                        </button>
-                    </a>
-                    <a href="{{route('register')}}">
-                        <button type="button" class="rounded-md bg-indigo-800 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Register
-                        </button>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <hr>
-        <div class="justify-center w-full mx-auto bg-indigo-800">
-            <div class="flex flex-col w-full px-8 py-2.5 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
-                <p class="text-xs text-white">
-                    Jl. Telekomunikasi No. 5, Terusan Buah Batu, Bandung, 40257
-                </p>
-            </div>
-        </div>
-    </nav>
-
+    @include('layouts.navbar')
     <section>
         <div class="px-8 py-16 mx-auto md:px-12 lg:px-32 max-w-7xl">
             <div>
-                <h1 class="text-xl font-semibold tracking-tighter text-indigo-900 lg:text-6xl text-balance">
+                <h1 class="text-xl font-bold tracking-tighter text-indigo-900 lg:text-6xl text-balance">
                     Ekonomi Sejahtera
                     <span class="text-indigo-800">Bersama Koperasi Jaya</span>
                 </h1>
