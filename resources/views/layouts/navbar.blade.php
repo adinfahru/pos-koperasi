414ffaf19e687808d4c0d86ba55ab531378fc2ea
mhasbia09 <mhasbiao09@gmail.com>
2024-04-05 21:53:05 +0700

    minor update: adjustment pada font dan perubahan desain button login (navbar)

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,20 +1,20 @@
 <nav>
     <div class="justify-center w-full mx-auto">
-        <div class="flex flex-col w-full px-8 py-2 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
+        <div class="flex flex-col w-full px-8 py-4 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-7xl">
             <div class="flex flex-row items-center justify-between text-black">
-                <a class="inline-flex items-center gap-3 text-lg font-bold tracking-tight text-indigo-800" href="/">
+                <a class="inline-flex items-center gap-3 text-lg font-black tracking-tight text-indigo-800 lg:text-2xl" href="/">
                     <span>Koperasi Jaya</span>
                 </a>
             </div>
-            <div class="flex-col items-center text-xs gap-12 text-sm font-medium text-indigo-800 md:flex md:flex-row lg:p-0 md:mt-0">
-                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Beranda</a>
-                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Profile</a>
-                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Info Berita</a>
-                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_">Produk</a>
-                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="{{route('register')}}">Register</a>
+            <div class="flex-col items-center text-xs gap-14 text-sm font-semibold text-indigo-800 md:flex md:flex-row lg:p-0 md:mt-0">
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_" style="font-size: 14px;">Beranda</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_" style="font-size: 14px;">Profile</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_" style="font-size: 14px;">Info Berita</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="#_" style="font-size: 14px;">Produk</a>
+                <a class="hover:text-blue-600 focus:outline-none focus:text-gray-500 md:ml-auto" href="{{route('register')}}" style="font-size: 14px;">Register</a>
                 <a href="{{route('login')}}">
-                    <button type="button" class="rounded-md bg-indigo-800 px-6 py-1.5 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        Masuk
+                    <button type="button" class="rounded-md bg-transparent px-6 py-2 text-xs font-semibold text-indigo-800 shadow-sm hover:bg-indigo-800 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:transparent" style="font-size: 14px; border: 2px solid;">
+                        Login
                     </button>
                 </a>
             </div>
@@ -22,7 +22,7 @@
     </div>
     <hr>
     <div class="justify-center w-full mx-auto bg-indigo-800">
-        <div class="flex flex-col w-full px-8 py-1 mx-auto md:px-12 md:items-center md:justify-between md:flex-row lg:px-32 max-w-1xl">
+        <div class="flex flex-col w-full px-8 py-1 mx-auto md:px-14 md:items-center md:justify-between md:flex-row lg:px-52 max-w-1xl">
             <p class="text-xs text-white">
                 Jl. Telekomunikasi No. 5, Terusan Buah Batu, Bandung, 40257
             </p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
-    <title>Pos Koperasi</title>
-
+    <title>Pos Koperasi Jaya</title>
 </head>
 
 <body class="antialiased">
@@ -15,36 +14,28 @@
         <div class="px-8 py-16 mx-auto md:px-12 lg:px-32 max-w-7xl">
             <div>
                 <h1 class="text-xl font-bold tracking-tighter text-indigo-900 lg:text-6xl text-balance">
-                    Ekonomi Sejahtera
+                    Ekonomi Sejahtera,
                     <span class="text-indigo-800">Bersama Koperasi Jaya</span>
                 </h1>
-                <p class="mt-4 text-base font-medium text-indigo-800 text-balance">
-                    Control and added security. With decentralization, users have more
-                    control over their data and transactions, and the platform is less
-                    susceptible to malicious attacks.
-                </p>
             </div>
             <div class="space-y-2">
-                <div class="grid gap-2 mt-12 mb-8 lg:grid-flow-col-dense lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-1">
+                <div class="grid gap-4 mt-10 mb-28 lg:grid-flow-col-dense lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-1">
                     <div class="max-w-lg min-w-full mx-auto">
                         <div class="flex h-full">
-                            <div class="flex flex-col justify-center p-8 border bg-indigo-50 rounded-3xl max-w-none">
-                                <h2 class="font-medium text-gray-900">
-                                    Welcome to Koperasi Jaya, your gateway to economic prosperity and community empowerment.
-                                    Our platform stands as a beacon of hope, championing the ideals of shared prosperity and collaborative growth.
-                                    With the tagline "Ekonomi Sejahtera Bersama Koperasi Jaya," we are committed to fostering economic well-being and
-                                    ensuring a prosperous future for all.
+                            <div class="flex flex-col justify-center p-8 border bg-indigo-800 rounded-3xl max-w-none">
+                                <h2 class="font-medium text-white font-semibold text-justify">
+                                    Koperasi Jaya didirikan pada tanggal 8 Maret 2024 oleh mahasiswa Universitas Telkom dari program studi S1 Sistem Informasi, yaitu Aria Riezki Fhadila, Josia Fortino Pakpahan, Kevin Fritz Angelo, Mohammad Fahrudin, M Hasbi Asshiddiqqi, M. Rayhan Maulana Al-Rasyid, dan R. Rama Ahmad Waliyya.
+                                    Koperasi ini disahkan dengan Akte Badan Hukum No.9999 pada tanggal 10 Maret 2024 sebagai Koperasi Serba Usaha. Anggota dari Koperasi Jaya adalah karyawan tetap yang terdaftar pada BUMD.
                                 </h2>
-                                <p class="mt-4 text-sm font-medium text-gray-500 text-pretty">
-                                    Join us in our mission to build a brighter tomorrow, where economic prosperity is not just a dream but a reality for all.
-                                    Together, let's create a future of shared success and prosperity. Ekonomi Sejahtera Bersama Koperasi Jaya!
+                                <p class="mt-4 text-sm font-regular text-white text-pretty text-justify">
+                                    Bergabunglah dengan kami dalam misi untuk membuat hari esok yang lebih baik, kemakmuran ekonomi bukan hanya sekedar mimpi tetapi kenyataan bagi semua. Bersama-sama, mari kita ciptakan masa depan kesuksesan dan kemakmuran bersama. Ekonomi Sejahtera Bersama Koperasi Jaya!
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div class="lg:col-span-2">
                         <div>
-                            <div class="relative h-full p-2 overflow-hidden border rounded-3xl">
+                            <div class="relative h-full p-0 overflow-hidden border-rounded">
                                 <img src="{{ asset('images/Koperasi-Logo.png') }}" class="object-cover border shadow-2xl rounded-2xl" width="300" height="200">
                             </div>
                         </div>
@@ -52,7 +43,6 @@
                 </div>
                 <div class="justify-center w-full mx-auto">
                     <div class="bg-indigo-800 flex flex-col w-full px-8 py-6 mx-auto">
-
                         <div>
                             <div class="px-8 py-2 mx-auto md:px-12 lg:px-4 max-w-7xl">
                                 <div class="grid grid-cols-2 text-sm font-medium text-center text-gray-500 gap-x-6 gap-y-12 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16 text-balance">
@@ -104,10 +94,10 @@
                 <div class="text-center">
                 <h1 class=" mt-6 text-xl font-semibold tracking-tighter text-indigo-900 lg:text-6xl text-balance">
                     SEMUA KEBUTUHAN ANDA ADA DI
-                    <span class="text-indigo-800">KOPERASIJAYA</span>
+                    <span class="text-indigo-800 font-bold">Koperasi Jaya</span>
                 </h1>
                 <p class="mt-4 text-base font-medium text-indigo-800 text-balance">
-                    KOPERASIJAYA menyediakan segala kebutuhan anda terkait koperasi serba usaha. Mari kita lihat apa saja yang bisa dilakukan.
+                    Koperasi Jaya menyediakan segala kebutuhan anda terkait koperasi serba usaha. Mari kita lihat apa saja yang bisa dilakukan.
                 </p> <br>
             </div>
                 <div class="grid gap-2 lg:grid-flow-col-dense lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-3">
