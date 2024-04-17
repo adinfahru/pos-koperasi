2e32956f97c2c5bd95534d46014ce9c28c61b998
angelokevinn <angelokevinn.17@gmail.com>
2024-04-17 19:52:19 +0700

    Finalisasi Edit Product, Function Update Image (Kevin)

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -71,9 +71,17 @@ class ProductsController extends Controller
         $product->price = $request->price;
         $product->purchase = $request->purchase;
 
+        // Check if a new image file has been uploaded (Kevin)
+        if ($request->hasFile('image')) {
+            // Move the new image file to the desired directory
+            $request->file('image')->move('fotoProduk/', $request->file('image')->getClientOriginalName());
+            // Update the image attribute of the existing product with the new file name
+            $product->image = $request->file('image')->getClientOriginalName();
+        }
+        // Save the changes to the product
         $product->save();
 
-        return redirect()->route('products.index')->with('success', 'Product updated successfully');
+        return redirect()->route('products.index')->with('success', 'Product updated successfully');
     }
 
     /**
