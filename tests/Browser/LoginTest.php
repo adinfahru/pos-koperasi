01c64a2da7db1f1c0741c83ee11073159c9972ff
riezkiaria <aria.riezki@gmail.com>
2024-05-28 21:32:20 +0700

    Login Test

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel')
+                    ->clickLink('register')
+                    ->type('email','User1@gmai.com')
+                    ->type('password','User12345')
+                    ->press('Login');
+
+        });
+    }
+}
